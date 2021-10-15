9b1da8e3493aa0b33baee30468b309c36623de17
wechulimaven <mavenwechuli@gmail.com>
2021-10-15 21:50:54 +0300

    frontend

---
+++ layouts/AdminLayout.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Navbar from "../components/Navbars/AdminNavbars";
+import HeaderStats from "../components/Headers/HeaderStats";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+export default function Admin({ children }) {
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-blueGray-100">
+        <Navbar />
+        {/* Header */}
+        <HeaderStats />
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          {children}
+          {/* <FooterAdmin /> */}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ pages/admin/tables.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CardTable from "../../components/Cards/TableCard";
+import Admin from "../../layouts/AdminLayout";
+
+// components
+
+export default function Tables() {
+  return (
+    <>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full mb-12 px-4">
+          <CardTable />
+        </div>
+        <div className="w-full mb-12 px-4">
+          <CardTable color="dark" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+Tables.layout = Admin;
